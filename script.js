fc1ee35798bc63195b2a150acffcb4d89dfc588a
Remco Kroonen <99039459@mydavinci.nl>
2018-12-11 14:32:43 +0100

    objects gemaakt

---
+++ script.js
@@ -0,0 +1,18 @@
+function pizzas() {
+    //var pizzas = ["Margherita", "Marino", "Napoletana", "Quattro formaggi", "Alle melanzane", "Prosciutto", "Calzone", "Caponne", "Parma", "Slovenska"];
+    //document.getElementById("demo").innerHTML = pizzas[0];
+ }
+
+var = pizza1 {name:'margherita', price:'5,00', image:'PizzaMargherita.jpg'};
+var = pizza2 {name:'marino', price:'6,50', image:''};
+var = pizza3 {name:'napoletana', price:'8,99', image:''};
+var = pizza4 {name:'quattro formaggi', price:'9,95', image:''};
+var = pizza5 {name:'alle melanzane', price:'7,00', image:''};
+var = pizza6 {name:'prosciutto', price:'8,00', image:''};
+var = pizza7 {name:'calzone', price:'9,50', image:''};
+var = pizza8 {name:'caponne', price:'5,50', image:''};
+var = pizza9 {name:'parma', price:'6,00', image:''};
+var = pizza10 {name:'slovenska', price:'7,95', image:''};
+
+
+    
