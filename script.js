ff9ed088eb81e6a63fc106459f7cbe9dffa26e22
Remco Kroonen <99039459@mydavinci.nl>
2019-02-18 15:37:19 +0100

    nederlands naar engels begonnen

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 //console.log(pizzas[]);
-var gekozenpizzaprijs = [];
-var gekozentoppingprijs = [];
-var gekozenslice = 1;
-var gekozengrootteprijs = 1;
+var chosenpizzaprice = [];
+var chosentoppingprice = [];
+var chosenslice = 1;
+var chosensizeprice = 1;
 
 
-var allepizzas = [
+var allpizzas = [
 				{name:'margherita', price:'5.00', image:'images/PizzaMargherita.jpg'},
 				{name:'marino', price:'6.50', image:'images/marinos.jpg'},
 				{name:'napoletana', price:'8.99', image:'images/napoletana.jpg'},
@@ -20,7 +20,7 @@ var allepizzas = [
 
 	 ];
 
-var alletoppings = [
+var alltoppings = [
 				{name:'Extra kaas', price:'0.50'},
 				{name:'Spek', price:'0.99'},
 				{name:'Ham', price:'0.99'},
@@ -51,8 +51,8 @@ var alleslices = [
 
 function toonpizzas() {
 	var pizzatekst = 'Maak een keuze <br>';
-	for (var i = 0; i < allepizzas.length; i++) {
-		pizzatekst = pizzatekst + "<div class = 'pizzategel' ' onclick='prijspizza(this.id)' id='"+allepizzas[i].price+"'> <p>" + allepizzas[i].name + "</p> <img width= '152' height = '114' src='" + allepizzas[i].image + "'> <p>" + allepizzas[i].price + "</p> </div>";
+	for (var i = 0; i < allpizzas.length; i++) {
+		pizzatekst = pizzatekst + "<div class = 'pizzategel' ' onclick='prijspizza(this.id)' id='"+allpizzas[i].price+"'> <p>" + allpizzas[i].name + "</p> <img width= '152' height = '114' src='" + allpizzas[i].image + "'> <p>" + allpizzas[i].price + "</p> </div>";
 
 	}
 	document.getElementById("pizzas").innerHTML = pizzatekst;
@@ -62,8 +62,8 @@ function toonpizzas() {
 
 function toontoppings() {
 	var toppingtekst = 'Kies een topping <br> <ul id = "toppingslijst">';
-	for (var i = 0; i < alletoppings.length; i++) {
-		toppingtekst = toppingtekst +  "<li id= '"+alletoppings[i].price+"' onclick='prijstoppings(this.id)' >" + alletoppings[i].name + "(" + alletoppings[i].price + ")</li>"
+	for (var i = 0; i < alltoppings.length; i++) {
+		toppingtekst = toppingtekst +  "<li id= '"+alltoppings[i].price+"' onclick='prijstoppings(this.id)' >" + alltoppings[i].name + "(" + alltoppings[i].price + ")</li>"
 
 	}
 	toppingtekst = toppingtekst + "</ul>"
@@ -103,7 +103,7 @@ function prijspizza(clicked_id){
 	}
 	var element = document.getElementById(clicked_id);
 	element.classList.add("pizzategelgekozen");
-	gekozenpizzaprijs = clicked_id;
+	chosenpizzaprice = clicked_id;
 	toonprijs();
 }
 
@@ -118,13 +118,12 @@ function prijstoppings(clicked_id){
 	}
 	var ul = document.getElementById("toppingslijst");
 	var items = ul.getElementsByClassName("gekozentekst");
-	gekozentoppingprijs = 0;
-	debugger;
+	chosentoppingprice = 0;
 	for (var i = 0; i < items.length; i++){
-		gekozentoppingprijs += Number(items[i].id);
+		chosentoppingprice += Number(items[i].id);
 		//items[i].classList.remove("gekozentekst");
 	}
-	//gekozentoppingprijs = clicked_id;
+	//chosentoppingprice = clicked_id;
 	toonprijs();
 }
 
@@ -136,7 +135,7 @@ function prijsgrootte(clicked_id){
 	}
 	var element = document.getElementById(clicked_id);
 	element.classList.add("gekozentekst");
-	gekozengrootteprijs = clicked_id;
+	chosensizeprice = clicked_id;
 	toonprijs();
 }
 
@@ -148,12 +147,12 @@ function prijslice(clicked_id){
 	}
 	var element = document.getElementById(clicked_id);
 	element.classList.add("gekozentekst");
-	gekozenslice = clicked_id;
+	chosenslice = clicked_id;
 	toonprijs();
 }
 
 function toonprijs(){
-	var opgeteld = ((Number(gekozenpizzaprijs) + Number(gekozentoppingprijs)) * Number(gekozengrootteprijs)) * Number(gekozenslice);
+	var opgeteld = ((Number(chosenpizzaprice) + Number(chosentoppingprice)) * Number(chosensizeprice)) * Number(chosenslice);
 
 	document.getElementById("totaalprijs").innerHTML = "<h1> €" + opgeteld.toFixed(2) + "</h1>";
 
